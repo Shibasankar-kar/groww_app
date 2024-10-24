de78d97296c925c63e15edaa30cd8d438ccf77f8
Yadhuraj <automate0323@gmail.com>
2024-10-24 19:57:53 +0530

    added theme and navigation setup configuration file also added

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,32 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {navigationRef} from '../utils/NavigationUtil';
+import { useCustomTheme } from './Theme';
+import MainNavigator from './MainNavigator';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation: React.FC = () => {
+  const theme = useCustomTheme();
+  const MyTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: theme.colors.background,
+      text: theme.colors.text,
+      card: theme.colors.card,
+      border: theme.colors.border,
+      notification: theme.colors.notification,
+      primary: theme.colors.primary,
+    },
+  };
+  return (
+    <NavigationContainer ref={navigationRef} theme={MyTheme}>
+      <MainNavigator />
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
